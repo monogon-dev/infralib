67a630b378c863ea1bb93e88eaf4b6a37ae05a13
Leopold Schabel <leo@nexantic.com>
2022-06-08 20:03:14 +0200

    k8s: add build tag to deps.go
    This avoids confusing tooling. Drive-by fix a malformed import.
    
    Change-Id: Ib60a9b12dab9181b10dd7b8c8da8b0cedd9eb01d
    Reviewed-on: https://review.monogon.dev/c/infralib/+/760
    Reviewed-by: Sergiusz Bazanski <serge@monogon.tech>

--- k8s/deps.go
+++ k8s/deps.go
@@ -1,5 +1,7 @@
 // Copyright 2020 The Monogon Project Authors.
 // SPDX-License-Identifier: Apache-2.0
+//
+//go:build imports
 
 package k8s
 
@@ -9,7 +11,7 @@ import (
 	_ "github.com/prometheus/prometheus/pkg/rulefmt"
 	_ "k8s.io/api/apps/v1"
 	_ "k8s.io/api/core/v1"
+	_ "k8s.io/api/networking/v1"
 	_ "k8s.io/api/rbac/v1"
 	_ "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
-	- "k8s.io/api/networking/v1"
 )
